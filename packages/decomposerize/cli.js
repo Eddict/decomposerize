a3507293e5d85566d36dfb5457b09ecd7dea85db
Eddict <microsoft@eddict.nl>
2026-03-12 06:04:49 +0100

    Analyze: update cli.js

--- packages/decomposerize/cli.js
+++ packages/decomposerize/cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 /* eslint-disable */
 
-const decomposerize = require('./dist/decomposerize');
+const path = require('path');
+const decomposerize = require(path.join(__dirname, 'dist', 'decomposerize'));
 
 var composeFile = '';
 process.stdin.on('data', function(d) {
